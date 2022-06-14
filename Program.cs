6e9d7028db697e1558cc10d452060bcbf32a0bcf
DT417 <DT417@IND-CHN-LT11834>
2022-06-14 16:17:08 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Training
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a Number :");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int r = 0, temp = 0;
+            int OrginalNum = n;
+
+            while (n != 0)
+            {
+                r = n % 10;
+                temp = temp * 10 + r;
+                n = n / 10;
+            }
+            if (OrginalNum==temp)
+            {
+                Console.WriteLine("Palindrome");
+            }
+            else
+            {
+                Console.WriteLine("Not Palindrome");
+            }
+            Console.ReadLine();
+        }
+    }
+}
+
